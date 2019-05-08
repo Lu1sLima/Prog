90d21eab4d6fedc9fb1ef0fc70c89415f1993420
Lu1sPROG <50342687+Lu1sPROG@users.noreply.github.com>
2019-05-08 13:21:52 -0300

    Add files via upload

---
+++ Bib.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex8;
+
+/**
+ *
+ * @author 19111170
+ */
+public class Bib {
+    public boolean numeroPerfeito (int num){
+        if (somaDivPro(num) == num) { return true;}
+        else {return false;}
+    }    
+    public int somaDiv(int num){
+        
+    }
+}
+
+
+//somaDivPro retorna a soma dos divisores exceto o próprio número
+//somaDiv retorna a soma dos divisores
