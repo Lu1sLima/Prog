bae996caf05c6dbab239451c28fe93cf8de88ff5
Lu1sPROG <50342687+Lu1sPROG@users.noreply.github.com>
2019-05-08 17:34:59 -0300

    Add files via upload

--- Ex8.java
+++ Ex8.java
@@ -1,26 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package ex8;
 
 /**
- *
- * @author 19111170
+ * Mostra os 4 primeiros números perfeitos (Não foi até o 5 pq seria um loop muito grande.
+ * 
+ * @author (Luís) 
+ * @version (08/05/19)
  */
-public class Ex8 {
-    public static void main(String[] args) {
-        //int num = 0; // começa em 0 e vai até 999
-        for (int num = 6; num <= 999; num++) {
-            int contador = 0;
-            if (numPerfeito(num)){ // sei qe o método retorna boolean poderia ser if (numPerfeito(num) == true)
-                contador++;
-                System.out.println(num);
-            }
-            
+public class Ex8
+{
+    public static void main (String []args){
+        System.out.println("\f");
+        for (int num = 6; num <=  100000; num++){
+            if (testePerfeito(num)){System.out.println(num);}
         }
-        
+    }
     
-}
+    
+       public static int somaDivPro (int num){
+        int soma = 0;
+        for (int i = num/2; i >= 1; i--) { // coloquei num/2 pq os divisores de um número N estão até a METADE desse número. (e assim, não soma o próprio N)
+            if (num % i == 0){soma +=i;} // testando se i é divisor do número, se sim, guarda na variável auxiliar que recebe auxiliar+i
+        }
+        return soma;//retorna a variável axiliar, somando os divisores de num
+    }
+    
+    public static boolean testePerfeito (int num){// método para ver se o número é perfeito
+        if (somaDivPro(num) == num){return true;} // se a soma dos divisores for igual ao número, então ele é um número perfeito
+        else{return false;}
+    }
+    
 }
