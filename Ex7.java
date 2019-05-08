0fd0b7bf3337c02407929bcc4a24504cfdeeee2f
Lu1sPROG <50342687+Lu1sPROG@users.noreply.github.com>
2019-05-08 13:22:19 -0300

    Add files via upload

---
+++ Ex7.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex7.newpackage;
+
+/**
+ *
+ * @author 19111170
+ */
+public class Ex7 {
+    public static void main (String[]args){
+        int num = 7;
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(num * i);
+            
+        }
+    }
+    
+}
