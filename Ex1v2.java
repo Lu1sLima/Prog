04900634ff41c43a84ab5c5401d88a58324791c0
Lu1sPROG <50342687+Lu1sPROG@users.noreply.github.com>
2019-05-08 22:58:19 -0300

    Add files via upload

---
+++ Ex1v2.java
@@ -0,0 +1,25 @@
+
+/**
+ * Escreva a descrição da classe Ex1v2 aqui.
+ * 
+ * @author (seu nome) 
+ * @version (número de versão ou data)
+ */
+
+import java.util.Scanner;
+public class Ex1v2{
+	public static void main(String[]args){
+		Scanner input = new Scanner(System.in);
+		System.out.println("\f");
+		int contador = 0;
+		int num = 0;
+		int soma = 0;
+		System.out.println("Digite um número inteiro: ");
+		num = input.nextInt();
+
+		for (contador = num; contador >= 1; contador--){
+		    soma = contador+soma;
+		  }
+		  System.out.println(soma);
+	}
+}
