9a6f3be562117d65c1fc7b394dbe4ef8eb55a00e
Lu1sPROG <50342687+Lu1sPROG@users.noreply.github.com>
2019-08-12 11:02:58 -0300

    Add files via upload

---
+++ Cofrinho.java
@@ -0,0 +1,16 @@
+public class Cofrinho{
+    protected int posicao = 0;
+    protected Moeda [] cofrinho;
+
+    public Cofrinho(Moeda Qualmoeda){
+        cofrinho = new Moeda[10];
+        this.cofrinho[posicao] = Qualmoeda;
+        this.posicao++;
+    }
+
+    public
+
+
+
+
+}
